e70ddb22d48ddff0f6326d273c0f183b167e58ba
OkVurb <85900298+OkVurb@users.noreply.github.com>
2026-08-31 17:46:04 -0700

    Implement Warp Pipe sub-rooms

--- src/main/java/com/studio/planeshift/common/block/WarpPipeBlock.java
+++ src/main/java/com/studio/planeshift/common/block/WarpPipeBlock.java
@@ -39,4 +39,33 @@ public class WarpPipeBlock extends Block {
         }
         return InteractionResult.SUCCESS;
     }
+
+    @Override
+    public void stepOn(Level level, BlockPos pos, BlockState state, net.minecraft.world.entity.Entity entity) {
+        if (!level.isClientSide() && entity instanceof ServerPlayer player && player.isCrouching()) {
+            // Determine if we are going down or up
+            // Using a simple logic: if we are high up, go down. If low, go up.
+            // But an easier way is to check blockstate or just use a fixed offset.
+            // Let's use an offset of 50. If there's a WarpPipeBlock 50 blocks above, we go up. Otherwise go down.
+            boolean isReturnPipe = level.getBlockState(pos.above(50)).getBlock() == this;
+            BlockPos targetPos = isReturnPipe ? pos.above(50) : pos.below(50);
+
+            if (!isReturnPipe && level.getBlockState(targetPos).isAir()) {
+                // Generate a simple platform and return pipe
+                level.setBlockAndUpdate(targetPos, this.defaultBlockState());
+                for (int x = -1; x <= 1; x++) {
+                    for (int z = -1; z <= 1; z++) {
+                        level.setBlockAndUpdate(targetPos.below().offset(x, 0, z), net.minecraft.world.level.block.Blocks.STONE.defaultBlockState());
+                    }
+                }
+                // clear some space above
+                for (int y = 1; y <= 3; y++) {
+                    level.setBlockAndUpdate(targetPos.above(y), net.minecraft.world.level.block.Blocks.AIR.defaultBlockState());
+                }
+            }
+
+            player.teleportTo(targetPos.getX() + 0.5, targetPos.getY() + 1.0, targetPos.getZ() + 0.5);
+        }
+        super.stepOn(level, pos, state, entity);
+    }
 }
